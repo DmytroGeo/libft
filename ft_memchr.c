86f0268814e86b11073c59480b26507d3eb3177f
Dmytro Georgiyenko <dgeorgiy@c2r7s6.42london.com>
2024-06-24 11:09:52 +0100

    memchr

---
+++ ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 18:07:00 by dgeorgiy          #+#    #+#             */
+/*   Updated: 2024/06/24 11:02:22 by dgeorgiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	k;
+	unsigned char *str;
+	
+	k = 0;
+	str = (unsigned char*)s;
+	
+	while (k < n)
+	{
+		if (str[k] == c)
+			return(str + k);
+		k++;
+	}
+	return(NULL);
+}
