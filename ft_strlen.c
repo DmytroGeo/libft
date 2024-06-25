c9e9e070a896e4a2b4c7737e110d885a6396fdb4
Dmytro Georgiyenko <dgeorgiy@student.42london.com>
2024-06-25 19:32:26 +0100

    moved tester function into main.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 16:15:19 by dgeorgiy          #+#    #+#             */
-/*   Updated: 2024/06/24 13:44:47 by dgeorgiy         ###   ########.fr       */
+/*   Updated: 2024/06/25 14:35:19 by dgeorgiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	n;
 
 	n = 0;
-	while (s[n] != '\0')
+	while (s[n])
 		n++;
 	return (n);
 }
-/*
-size_t ft_strlen2(const char *s)
-{
-	if (s[0] == '\0')
-		return(0);
-	return(1 + ft_strlen2(s+1));
-}
-*/
-/*int	main(void)
-{
-	printf("%lu", ft_strlen2("hello"));
-	return(0);
-}
-*/
