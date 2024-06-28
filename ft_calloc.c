2cd8cb9738020f46035b38fabd15a15d2234aaed
Dmytro Georgiyenko <dgeorgiy@student.42london.com>
2024-06-28 10:54:33 +0100

    passed norminette

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,22 @@
 /*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 18:34:38 by dgeorgiy          #+#    #+#             */
-/*   Updated: 2024/06/25 13:55:42 by dgeorgiy         ###   ########.fr       */
+/*   Updated: 2024/06/28 10:53:13 by dgeorgiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
-{	
+{
 	void	*ptr;
 
 	ptr = malloc(nmemb * size);
-
 	if (!ptr)
-		return(NULL);
+		return (NULL);
 	else if (size != 0 && ((SIZE_MAX) / size) < nmemb)
-		return(NULL);
+		return (NULL);
 	else
 		ft_bzero(ptr, nmemb * size);
-	return(ptr);
+	return (ptr);
 }
