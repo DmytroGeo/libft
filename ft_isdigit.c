4096741467bfa0181f6c352d35084a37dd381a0b
Dmytro Georgiyenko <dgeorgiy@student.42london.com>
2024-06-28 16:09:25 +0100

    passed norm

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:14:42 by dgeorgiy          #+#    #+#             */
-/*   Updated: 2024/06/12 20:16:10 by dgeorgiy         ###   ########.fr       */
+/*   Updated: 2024/06/28 16:08:12 by dgeorgiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,6 @@
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return(1);
-	return(0);
+		return (1);
+	return (0);
 }
-/*
-int	main(int ac, char **av)
-{
-	if (ac > 1)
-	{
-		char i = av[1][0];
-		if (ft_isdigit(i))
-			printf("Yes\n");
-		else
-			printf("No\n");
-	}
-	return(0);
-}
-*/
