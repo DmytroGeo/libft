d3f28f91165d714f0ebf4cf1e69e10bb105aad8e
Dmytro Georgiyenko <dgeorgiy@student.42london.com>
2024-07-15 15:33:56 +0100

    needs fixing

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,10 +6,19 @@
 /*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:16:30 by dgeorgiy          #+#    #+#             */
-/*   Updated: 2024/07/08 16:17:11 by dgeorgiy         ###   ########.fr       */
+/*   Updated: 2024/07/15 15:31:31 by dgeorgiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*current;
+	
+	current = *lst;
+	while (current != NULL)
+		current = current->next;
+	current->next = new;
+	new->next = NULL;
+}
