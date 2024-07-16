964661593985a297b2a6f6dc3ab898cfee52cf80
Dmytro Georgiyenko <dgeorgiy@student.42london.com>
2024-07-16 15:23:27 +0100

    passed norm and tests

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,10 +6,24 @@
 /*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:18:22 by dgeorgiy          #+#    #+#             */
-/*   Updated: 2024/07/08 16:22:40 by dgeorgiy         ###   ########.fr       */
+/*   Updated: 2024/07/16 12:28:41 by dgeorgiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*current;
+
+	current = *lst;
+	if (current == NULL || !del)
+		return ;
+	while (current != NULL)
+	{
+		*lst = current->next;
+		(del)(current->content);
+		free(current);
+		current = *lst;
+	}
+}
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,10 +6,19 @@
 /*   By: dgeorgiy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:19:15 by dgeorgiy          #+#    #+#             */
-/*   Updated: 2024/07/08 16:19:42 by dgeorgiy         ###   ########.fr       */
+/*   Updated: 2024/07/16 12:50:30 by dgeorgiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst == NULL || !f)
+		return ;
+	while (lst != NULL)
+	{
+		(f)(lst->content);
+		lst = lst->next;
+	}
+}
